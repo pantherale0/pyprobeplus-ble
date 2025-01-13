4b230a92145b4b14738a48c77114f6104073acf1
Jordan <jordan@hrvy.uk>
2025-01-13 22:51:10 +0000

    update module init

--- src/pyprobeplus_ble/__init__.py
+++ src/pyprobeplus_ble/__init__.py
@@ -5,4 +5,11 @@ __version__ = "1.1.1"
 
 from bleak_retry_connector import get_device
 
-from .pyprobeplus_ble import ProbePlusBLE
+from .pyprobeplus_ble import ProbePlusBLE, ProbeState, RelayState
+
+__all__ = [
+    "ProbePlusBLE",
+    "ProbeState",
+    "RelayState",
+    "get_device"
+]
